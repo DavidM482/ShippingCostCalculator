92f56c5f60ff6090f12ad86d926855bcb931b1be
DavidM482 <martin.613901@swl.k12.oh.us>
2024-09-25 08:43:11 -0400

    There needs an update

--- src/ShippingCostCalculator.java
+++ src/ShippingCostCalculator.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
 
-public static void main(String[] args) {
-    
+public class ShippingCostCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //variables
+        int maxShippingPrice = 100;
+        double totalPrice;
+        double itemPrice;
+        double shippingPercent = 0.02;
+
+        //output
+        System.out.println("What is the price of your item?");
+        if (scan.hasNextDouble()) {
+            itemPrice = scan.nextDouble();
+
+            //decision
+            if (itemPrice <= maxShippingPrice) {
+                totalPrice = itemPrice + (itemPrice * shippingPercent);
+                System.out.println("Your total cost is " + totalPrice + ".");
+            } else {
+                totalPrice = itemPrice;
+                System.out.println("Your total cost is " + totalPrice + ". You had free shipping.");
+            }
+        } else {
+            System.out.println("You have entered an invalid value.");
+        }
+    }
 }
